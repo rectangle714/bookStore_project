75fe884fd9854471f86d7ff61c36229805eb8c3d
rectangle714@naver.com <rectangle714@naver.com>
2024-02-28 16:08:17 +0900

    배치 틀 개발 완료 - 20240228

--- bs_project/bs_batch/src/main/java/com/bs_batch/BsBatchApplication.java
+++ bs_project/bs_batch/src/main/java/com/bs_batch/BsBatchApplication.java
@@ -2,16 +2,16 @@ package com.bs_batch;
 
 import config.JpaAuditingConfig;
 import config.QueryDslConfig;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"core","com.bs_batch"})
-@EntityScan(basePackages = {"core"})
-@EnableJpaRepositories(basePackages = {"core"})
 @Import({JpaAuditingConfig.class, QueryDslConfig.class})
+@SpringBootApplication(scanBasePackages = {"core","com.bs_batch"})
+@EntityScan(basePackages = {"core"}) @EnableJpaRepositories(basePackages = {"core"})
 public class BsBatchApplication {
 
 	public static void main(String[] args) {
--- bs_project/bs_batch/src/main/java/com/bs_batch/config/JobConfiguration.java
+++ bs_project/bs_batch/src/main/java/com/bs_batch/config/JobConfiguration.java
@@ -6,42 +6,75 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.*;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
 @Configuration
-@EnableBatchProcessing
 @RequiredArgsConstructor
+@EnableBatchProcessing
 public class JobConfiguration {
 
     private final MemberRepository memberRepository;
+    private final PlatformTransactionManager transactionManager;
 
-//    @Bean
-//    public Job expiredMemberJob(JobRepository jobRepository, Step step) {
-//        return new JobBuilder("selectExpiredMember", jobRepository)
-//                .preventRestart()
-//                .start(step)
-//                .build();
-//    }
-//
-//    @Bean
-//    public Step expiredMemberJobStep(JobRepository jobRepository, Tasklet myTasklet, PlatformTransactionManager transactionManager) {
-//        return new StepBuilder("selectExpiredMember", jobRepository)
-//                .chunk(100, transactionManager)
-//                .build();
-//    }
+    @Bean
+    public Job expiredMemberJob(JobRepository jobRepository, Step step) {
+        return new JobBuilder("selectExpiredMemberJob", jobRepository)
+                .preventRestart()
+                .start(expiredMemberStep(jobRepository))
+                .build();
+    }
 
-//    @Bean
-//    public ListItemReader<Member>
+    @Bean
+    public Step expiredMemberStep(JobRepository jobRepository) {
+        return new StepBuilder("expiredMemberStep", jobRepository)
+                .<Member, Member>chunk(100, transactionManager)
+                .reader(expiredMemberReader())
+                .processor(expiredMemberProcessor())
+                .writer(expiredMemberWriter())
+                .build();
+    }
+
+    @Bean
+    @StepScope
+    public ListItemReader<Member> expiredMemberReader() {
+        log.info("****** 사용자 조회 시작 ******");
+        List<Member> expiredMemberList = new ArrayList<>();
+        expiredMemberList = memberRepository.findAll();
+        log.info("****** 사용자 조회 끝 ******");
+        return new ListItemReader<>(expiredMemberList);
+    }
+
+    public ItemProcessor<Member, Member> expiredMemberProcessor() {
+        return member -> {
+            log.info(member.getEmail());
+            return member;
+        };
+    }
+
+    public ItemWriter<Member> expiredMemberWriter() {
+        log.info("********** 사용자 writer 시작 **********");
+        return memberList -> {
+            memberList.forEach(
+                    member -> log.info(member.getEmail())
+            );
+        };
+    }
 
 }
