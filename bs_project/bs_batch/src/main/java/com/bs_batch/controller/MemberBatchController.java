9f36c4da5a03caa36fa27f155aaedfdf9fd59f8d
rectangle714@naver.com <rectangle714@naver.com>
2024-03-23 19:55:33 +0900

    core gradle 커밋 - 20240323

---
+++ bs_project/bs_batch/src/main/java/com/bs_batch/controller/MemberBatchController.java
@@ -0,0 +1,26 @@
+package com.bs_batch.controller;
+
+import com.bs_batch.config.BatchScheduler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/batch")
+@RequiredArgsConstructor
+public class MemberBatchController {
+    private final BatchScheduler batchScheduler;
+
+    /* 계정 만료 사용자 배치 수동 실행 */
+    @PostMapping("/expiredMember")
+    public ResponseEntity<String> expiredMemberBatch() {
+        if ("success".equals(batchScheduler.runExpiredMemberJob())) {
+            return ResponseEntity.ok("success");
+        } else {
+            return ResponseEntity.internalServerError().body("fail");
+        }
+    }
+
+}
